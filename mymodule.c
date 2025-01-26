af93963b1cf62d8b99f756826d82665d9dbee1da
ducruetl1 <leo.ducruet@etu.univ-grenoble-alpes.fr>
2025-01-26 15:05:15 +0100

    Error message when major number not attributed

--- mymodule.c
+++ mymodule.c
@@ -1,5 +1,4 @@
 #include "linux/fs.h"
-#include "linux/kdev_t.h"
 #include "linux/kern_levels.h"
 #include "linux/printk.h"
 #include "linux/stat.h"
@@ -14,37 +13,41 @@ int random_int = 0;
 
 int notify_param(const char *val, const struct kernel_param *kp)
 {
-	int res = param_set_int(val, kp);
-	if (res == 0) {
-		printk(KERN_INFO "mymodule: Callback function called...\n");
-		printk(KERN_INFO "mymodule: New value of random_int = %d\n", random_int);
-		return 0;
-	}
-	return -1;
+				int res = param_set_int(val, kp);
+				if (res == 0) {
+								printk(KERN_INFO "mymodule: Callback function called...\n");
+								printk(KERN_INFO "mymodule: New value of random_int = %d\n", random_int);
+								return 0;
+				}
+				return -1;
 }
 
 struct kernel_param_ops my_param_ops =
 {
-	.set = &notify_param,
-	.get = &param_get_int,
+				.set = &notify_param,
+				.get = &param_get_int,
 };
 
 module_param_cb(random_int, &my_param_ops, &random_int, S_IRUGO|S_IWUSR);
 
 static int __init mod_init(void) 
 {
-	alloc_chrdev_region(&device_number, 0, 1, "mydriver");
-	printk(KERN_INFO "mymodule: Device major number : %d\n", MAJOR(device_number));
-	printk(KERN_INFO "mymodule: Device minor number : %d\n", MINOR(device_number));
-	printk(KERN_INFO "mymodule: random_int = %d\n", random_int);
-	printk(KERN_INFO "mymodule: My kernel module has been inserted");
-	return 0;	
+				if(alloc_chrdev_region(&device_number, 0, 1, "mydriver") < 0)
+				{
+								printk(KERN_ERR "mymodule: Can't allocate major number for device 1\n");
+								return -1;
+				}
+				printk(KERN_INFO "mymodule: Device major number : %d\n", MAJOR(device_number));
+				printk(KERN_INFO "mymodule: Device minor number : %d\n", MINOR(device_number));
+				printk(KERN_INFO "mymodule: random_int = %d\n", random_int);
+				printk(KERN_INFO "mymodule: My kernel module has been inserted");
+				return 0;	
 }
 
 static void __exit mod_exit(void) 
 {
-	unregister_chrdev_region(device_number, 1);
-	printk(KERN_INFO "mymodule: My kernel module has been removed");
+				unregister_chrdev_region(device_number, 1);
+				printk(KERN_INFO "mymodule: My kernel module has been removed");
 }
 
 module_init(mod_init);
