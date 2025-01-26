039160fdb2c4b102861759b369b6c3adcd69b040
ducruetl1 <leo.ducruet@etu.univ-grenoble-alpes.fr>
2025-01-26 16:28:34 +0100

    Added dynamic struct class and device creation

--- mymodule.c
+++ mymodule.c
@@ -1,3 +1,5 @@
+#include "linux/device/class.h"
+#include "linux/err.h"
 #include "linux/fs.h"
 #include "linux/kern_levels.h"
 #include "linux/printk.h"
@@ -7,16 +9,21 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/moduleparam.h>
+#include <linux/device.h>
+#include <linux/kdev_t.h>
 
-dev_t device_number = 0;
+dev_t dev = 0;
+static struct class *dev_class;
+
 int random_int = 0;
 
+/* Callback function for random_int*/
 int notify_param(const char *val, const struct kernel_param *kp)
 {
 				int res = param_set_int(val, kp);
 				if (res == 0) {
-								printk(KERN_INFO "mymodule: Callback function called...\n");
-								printk(KERN_INFO "mymodule: New value of random_int = %d\n", random_int);
+								pr_info("mymodule: Callback function called...\n");
+								pr_info("mymodule: New value of random_int = %d\n", random_int);
 								return 0;
 				}
 				return -1;
@@ -32,22 +39,46 @@ module_param_cb(random_int, &my_param_ops, &random_int, S_IRUGO|S_IWUSR);
 
 static int __init mod_init(void) 
 {
-				if(alloc_chrdev_region(&device_number, 0, 1, "mydriver") < 0)
+				/* Allocating major number */
+				if(alloc_chrdev_region(&dev, 0, 1, "mydriver") < 0)
 				{
-								printk(KERN_ERR "mymodule: Can't allocate major number for device 1\n");
+								pr_err("mymodule: Can't allocate major number for device 1\n");
 								return -1;
 				}
-				printk(KERN_INFO "mymodule: Device major number : %d\n", MAJOR(device_number));
-				printk(KERN_INFO "mymodule: Device minor number : %d\n", MINOR(device_number));
-				printk(KERN_INFO "mymodule: random_int = %d\n", random_int);
-				printk(KERN_INFO "mymodule: My kernel module has been inserted");
-				return 0;	
+				pr_info("mymodule: Device major number : %d\n", MAJOR(dev));
+				pr_info("mymodule: Device minor number : %d\n", MINOR(dev));
+
+				/* Creating struct class */
+				dev_class = class_create("mymodule");
+				if (IS_ERR(dev_class)) {
+								pr_err("mymodule: Can't create struct class\n");
+								goto err_class;
+				}
+
+				/* Creating device */
+				if (IS_ERR(device_create(dev_class, NULL, dev, NULL, "mydriver_device"))) {
+								pr_err("mymodule: Can't create device\n");
+								goto err_device;
+				}
+
+				pr_info("mymodule: random_int = %d\n", random_int);
+				pr_info("mymodule: My kernel module has been inserted\n");
+				return 0;
+
+err_device:
+				class_destroy(dev_class);
+err_class:
+				unregister_chrdev_region(dev, 1);
+				return -1;
+
 }
 
 static void __exit mod_exit(void) 
 {
-				unregister_chrdev_region(device_number, 1);
-				printk(KERN_INFO "mymodule: My kernel module has been removed");
+				device_destroy(dev_class, dev);
+				class_destroy(dev_class);
+				unregister_chrdev_region(dev, 1);
+				pr_info("mymodule: My kernel module has been removed\n");
 }
 
 module_init(mod_init);
